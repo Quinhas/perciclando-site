4b06ab114299abc338afc247a688126896e21486
Lucas Santana <ln.santana08@gmail.com>
2022-07-05 16:53:44 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Perciclando
+
+Project under development
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+# Clone the project
+git clone git@github.com:Quinhas/perciclando-site.git
+
+# Enter the directory
+cd perciclando-site
+
+# Install the dependencies
+yarn
+
+# Start the project
+yarn dev
+```
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Perciclando</title>
+        <meta name="description" content="Perciclando" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      ;
+    </>
+  );
+}
+
+export default MyApp;
